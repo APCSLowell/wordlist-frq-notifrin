ba52d028fc9c8011a575c7ebe42c8b8a97dd1a44
notifrin <80722932+notifrin@users.noreply.github.com>
2025-04-15 11:13:32 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -8,30 +8,20 @@ public class WordList
   public void add(String word) { myList.add(word); }
   public String toString() { return myList.toString(); }
 
-  public int numWordsOfLength(int len)
-  {
-   int count = 0;
-    for(int i =0; i<myList.size(); i++) {
-    if(myList.get(i).length() == len) {
-      count++;
-
-    }
-
-    }
-    return count;
+  public int numWordsOfLength(int len){
+    int count = 0; i < wordlist.sized(); i++){
+      if (wordlist.get(i).size == len){
+        count++;
+      }
+    }return count;
 
   }
 
-  public void removeWordsOfLength(int len)
-  {
-    for(int i =0; i<myList.size(); i++) {
-    if(myList.get(i).length() == len) {
-    myList.remove(i);
-      i--;
-
+  public void removeWordsOfLength(int len){
+    for (int i = wordlist.length(); i >= 0; i--){
+if (wordlist.get.(i).size() == len){
+  wordlist.remove(i);
+}
+      return wordlist;
     }
-
-    }
-
-  }
 }
